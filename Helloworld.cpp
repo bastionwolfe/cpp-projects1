bb9e6335182c08a7644fbd475e9b93765ca8ced7
zach <zmusic2849@gmail.com>
2025-08-08 23:09:14 -0600

    added helloworld to finish off first week of project idea

---
+++ Helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// This program is the first foray into C++ programming. 
+
+// It uses two ways to print to the console \n and endl.
+
+
+int main() {
+	std::cout << "Hello, Universe!\n";
+	std::cout << "Bonjour, le monde aussi!" << std::endl;
+	return 0;
+}
