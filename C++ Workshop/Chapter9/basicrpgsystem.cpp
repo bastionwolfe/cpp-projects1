d311e08025b87a208825b6b96db5ae3b65c8da6d
bastionwolfe <166756081+bastionwolfe@users.noreply.github.com>
2025-09-23 20:41:18 -0600

    Update basicrpgsystem.cpp
    added characters to bottom since items and attacks need to be init first. changed some wordage to make brain hurt less.
    
    Works like a charm

--- C++ Workshop/Chapter9/basicrpgsystem.cpp
+++ C++ Workshop/Chapter9/basicrpgsystem.cpp
@@ -8,191 +8,166 @@
 //7. Test everything in a main function with a few different characters.
 // Finally, Must be made from scracth
 #include <iostream>
-#include <memory>
 #include <cstring>
 
-class Characters{
-    public:
-    
-    float getStrength() const {
-        return m_strengthMultiplier;
-    }
-    float getDefense() const {
-        return m_defenseMultiplier;
-    }
-    int getHealth() const {
-        return m_health;
-    }
-    const char* getName() const {
-        return m_name;
+class Attacks {
+public:
+    Attacks() : a_attackStat(0), a_attackName(nullptr) {}  // default
+
+    Attacks(int attack, const char* attackName) {
+        a_attackStat = attack;
+        a_attackName = new char[strlen(attackName) + 1];
+        strcpy(a_attackName, attackName);
     }
     
-    Attacks* getAttacks() const {
-        return m_attacks;
+    ~Attacks() { delete[] a_attackName; }
+
+    Attacks(const Attacks& other) {
+        a_attackStat = other.a_attackStat;
+        a_attackName = new char[strlen(other.a_attackName) + 1];
+        strcpy(a_attackName, other.a_attackName);
     }
-    
-    int getAttackCount() const {
-        return m_attackCount;
+    Attacks& operator=(const Attacks& other) {
+        if (this != &other) {
+            delete[] a_attackName;
+            a_attackStat = other.a_attackStat;
+            a_attackName = new char[strlen(other.a_attackName) + 1];
+            strcpy(a_attackName, other.a_attackName);
+        }
+        return *this;
     }
-    
-    Items* getItems() const {
-        return m_items;
+
+    int getAttackStat() const { return a_attackStat; }
+    const char* getAttackName() const { return a_attackName; }
+
+private:
+    int a_attackStat;
+    char* a_attackName;
+};
+
+class Items {
+public:
+    Items() : i_healStat(0), i_item_name(nullptr) {}  // default
+
+    Items(int heal, const char* name) {
+        i_healStat = heal;
+        i_item_name = new char[strlen(name) + 1];
+        strcpy(i_item_name, name);
     }
-    
-    
-    Characters(const char* name, int health, float strengthMultiplier, float defenseMultiplier) {
+    ~Items() { delete[] i_item_name; }
+
+    Items(const Items& other) {
+        i_healStat = other.i_healStat;
+        i_item_name = new char[strlen(other.i_item_name) + 1];
+        strcpy(i_item_name, other.i_item_name);
+    }
+    Items& operator=(const Items& other) {
+        if (this != &other) {
+            delete[] i_item_name;
+            i_healStat = other.i_healStat;
+            i_item_name = new char[strlen(other.i_item_name) + 1];
+            strcpy(i_item_name, other.i_item_name);
+        }
+        return *this;
+    }
+
+    int getHealStat() const { return i_healStat; }
+    const char* getItemName() const { return i_item_name; }
+
+private:
+    int i_healStat;
+    char* i_item_name;
+};
+
+class Characters {
+public:
+    Characters(const char* name, int health,
+               float strengthMultiplier, float defenseMultiplier,
+               Attacks* attacks, int attackCount,
+               Items* items, int itemCount)
+    {
         m_health = health;
         m_strengthMultiplier = strengthMultiplier;
         m_defenseMultiplier = defenseMultiplier;
-        
-        m_name = new char[strlen(name) +1];
+
+        m_name = new char[strlen(name) + 1];
         strcpy(m_name, name);
+
+        m_attackCount = attackCount;
+        m_attacks = new Attacks[attackCount];
+        for (int i = 0; i < attackCount; i++) {
+            m_attacks[i] = attacks[i];
+        }
+
+        m_itemCount = itemCount;
+        m_items = new Items[itemCount];
+        for (int i = 0; i < itemCount; i++) {
+            m_items[i] = items[i];
+        }
     }
-        
+
     ~Characters() {
         delete[] m_name;
         delete[] m_attacks;
         delete[] m_items;
     }
-    
-    Characters(const Characters& characters) {
-        m_health = characters.m_health;
-        m_strengthMultiplier = characters.m_strengthMultiplier;
-        m_defenseMultiplier = characters.m_defenseMultiplier;
-        
-        m_name = new char[strlen(characters.m_name) +1];
-        strcpy(m_name, characters.m_name);
+
+    void attack(Characters& target, int attackIndex) {
+        if (attackIndex < 0 || attackIndex >= m_attackCount) return;
+        int rawDamage = m_attacks[attackIndex].getAttackStat() * m_strengthMultiplier;
+        int finalDamage = rawDamage - (target.m_defenseMultiplier * 2); // simple formula
+        if (finalDamage < 0) finalDamage = 0;
+        target.m_health -= finalDamage;
+        std::cout << m_name << " attacks " << target.m_name
+                  << " with " << m_attacks[attackIndex].getAttackName()
+                  << " for " << finalDamage << " damage!\n";
     }
-    
-    Characters& operator=(const Characters& characters) {
-        if(this != &characters) {
-            m_health = characters.m_health;
-            m_strengthMultiplier = characters.m_strengthMultiplier;
-            m_defenseMultiplier = characters.m_defenseMultiplier;
-        
-            delete[] m_name;
-            m_name = new char[strlen(characters.m_name) +1];
-            strcpy(m_name, characters.m_name);
-        }
-        return *this;
+
+    void useItem(int itemIndex) {
+        if (itemIndex < 0 || itemIndex >= m_itemCount) return;
+        int heal = m_items[itemIndex].getHealStat();
+        m_health += heal;
+        std::cout << m_name << " uses " << m_items[itemIndex].getItemName()
+                  << " and heals " << heal << " HP!\n";
     }
-    
-    private:
+
+    void printStats() const {
+        std::cout << "Character: " << m_name
+                  << " | HP: " << m_health
+                  << " | STRx: " << m_strengthMultiplier
+                  << " | DEFx: " << m_defenseMultiplier << "\n";
+    }
+
+private:
     int m_health;
     char* m_name;
     float m_strengthMultiplier;
     float m_defenseMultiplier;
-    
+
     Attacks* m_attacks;
     int m_attackCount;
     Items* m_items;
-    int itemsCount;
-    
-    
+    int m_itemCount;
 };
 
-class Attacks{
-    public:
-    
-    int getAttackStat() const {
-        return a_attackStat;
-    }
-    const char* getAttackName() const{
-        return a_attackName;
-    }
-    
-    Attacks(int attack, char* attackName){
-        a_attackStat = attack;
-        
-        a_attackName = new char[strlen(attackName) +1];
-        strcpy(a_attackName, attackName);
-        
-    }
-    
-    ~Attacks(){
-        delete[] a_attackName;
-    }
-    
-    Attacks(const Attacks& attacks) {
-        a_attackStat = attacks.a_attackStat;
-        
-        a_attackName = new char[strlen(attacks.a_attackName) +1];
-        strcpy(a_attackName, attacks.a_attackName);
-    }
-    
-    Attacks& operator=(const Attacks& attacks) {
-        if (this != &attacks) {
-          a_attackStat =  attacks.a_attackStat;
-          
-          delete[] a_attackName;
-          a_attackName = new char[strlen(attacks.a_attackName) +1];
-          strcpy(a_attackName, attacks.a_attackName);
-        }
-        return *this;
-    }
-    
-    private:
-    int a_attackStat;
-    char* a_attackName;
-    
-};
+int main() {
+    Attacks warriorAttacks[] = { Attacks(10, "Slash"), Attacks(15, "Power Strike") };
+    Items warriorItems[] = { Items(20, "Potion"), Items(50, "Mega Potion") };
 
-class Items{
-    public:
-    
-    int getHealStat() {
-        return i_healStat;
-    }
-    
-    const char* getItemName() {
-        return i_item_name;
-    }
-    
-    Items(int heal, char* name) {
-        i_healStat = heal;
-        
-        i_item_name = new char[strlen(name) +1];
-        strcpy(i_item_name, name);
-    }
-    
-    ~Items() {
-        delete[] i_item_name;
-    }
-    
-    Items(const Items& items) {
-        i_healStat = items.i_healStat;
-        
-        i_item_name = new char[strlen(items.i_item_name) +1];
-        strcpy(i_item_name, items.i_item_name);
-    }
-    
-    Items& operator =(const Items& items) {
-        if (this != &items) {
-            i_healStat = items.i_healStat;
-            
-            delete[] i_item_name;
-            i_item_name = new char[strlen(items.i_item_name) +1];
-            strcpy(i_item_name, items.i_item_name);
-        }
-        return *this;
-    }
-    
-    private:
-    int i_healStat;
-    char* i_item_name;
-    
-};
+    Attacks mageAttacks[] = { Attacks(12, "Fireball"), Attacks(20, "Lightning") };
+    Items mageItems[] = { Items(30, "Herb") };
 
-void combatLog(){
-}
+    Characters warrior("Warrior", 100, 1.5f, 1.0f, warriorAttacks, 2, warriorItems, 2);
+    Characters mage("Mage", 80, 1.2f, 0.8f, mageAttacks, 2, mageItems, 1);
 
-void attack(Character &target){
-}
+    warrior.printStats();
+    mage.printStats();
 
-void printStats(){
-}
+    warrior.attack(mage, 0);
+    mage.attack(warrior, 1);
 
-int main(){
-    
-    return 0;
+    warrior.useItem(0);
+
+    warrior.printStats();
+    mage.printStats();
 }
